c249d4d21aec944f67dd3ecb53f0f61877c5c791
ErfanAbedinpour <erfan.wtf44@gmail.com>
2024-06-19 14:03:02 -0500

    added funtions for select engine and edit messages

--- actions/actions.js
+++ actions/actions.js
@@ -1,5 +1,6 @@
 const commponents = require("../commponents/commponents");
 const operatorToRedis = require("../saveActions/saveActions");
+const { messages } = require("../messages/messages");
 
 //start actions for bot
 exports.startActions = function (bot) {
@@ -7,7 +8,7 @@ exports.startActions = function (bot) {
     const chatId = msg.chat.id;
     bot.sendMessage(
       chatId,
-      "به روبات ترجمه خوش امدید",
+      messages.welcome + "\n" + messages.selectEngine,
       commponents.menueInlineKeyboard,
     );
   };
@@ -19,21 +20,31 @@ exports.callbackQueryActions = function (bot) {
     const cmd = query.data;
     const chatId = query.message.chat.id;
     const message_id = query.message.message_id;
-    if (cmd == "google") {
-      //code for google selecto
-      await operatorToRedis.addDataToRedis(chatId.toString(), { engine: cmd });
-      const message = "<h1>this is a header</h1><hr><p>this is paragraph</p>";
-      return bot.editMessageText(message, {
-        chat_id: chatId,
-        message_id,
-      });
-    } else if (cmd == "microsoft") {
-      //code for microsoft
-    } else if (cmd == "faraazin") {
-      //code for faraazni
-    } else if (cmd == "targoman") {
-      //code for targoman
-      return bot.sendMessage(chatId, "این گزینه فعلان در دسترس نیست");
+    const engines = ["google", "microsoft", "faraazin", "targoman"];
+    if (engines.includes(cmd)) {
+      if (cmd == "targoman") {
+        return bot.sendMessage(chatId, "targoman not avalibale now");
+      }
+      processActions(cmd, chatId, message_id, bot);
     }
   };
 };
+
+// user after select engine run this functions save user engine to redis and send dest lang
+async function engineActions(cmd, chatId, message_id, bot) {
+  await operatorToRedis.addDataToRedis(chatId.toString(), { engine: cmd });
+  editMessage(
+    `موتور ${cmd} با موفقیت انتخاب شد\n ${messages.sendDestLang}`,
+    chatId,
+    message_id,
+    bot,
+  );
+}
+
+//edit message function
+function editMessage(msg, chatId, message_id, bot) {
+  bot.editMessageText(msg, {
+    chat_id: chatId,
+    message_id,
+  });
+}
