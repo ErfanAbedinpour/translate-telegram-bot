1de1d4651586bbffb4a74e6e3c63dd2d65451f4d
ErfanAbedinpour <erfan.wtf44@gmail.com>
2024-06-19 14:01:42 -0500

    added some message to messages File

--- messages/messages.js
+++ messages/messages.js
@@ -1,5 +1,6 @@
 exports.messages = {
-  welcome: "خپش اومدی  به ربات",
+  welcome: "خوش اومدی ",
+  selectEngine: "لطفا موتور ترجمه خود را انتخاب نید",
   sendDestLang: `لطفا زبان مقصد خود را وارد کنید
 مثل (persian, english,french)`,
 };
