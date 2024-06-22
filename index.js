15b589028ac99e5cf1be6fc047c807fa274f86f2
ErfanAbedinpour <erfan.wtf44@gmail.com>
2024-06-22 08:45:08 -0400

    connect to proxy and error handler

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const TelegramBot = require("node-telegram-bot-api");
 const actions = require("./actions/actions");
+const { SocksProxyAgent } = require('socks-proxy-agent');
 require("dotenv").config();
 
+const agnet = new SocksProxyAgent('socks://localhost:2080')
+
 const botToken = process.env.BOT_TOKEN;
-const bot = new TelegramBot(botToken, { polling: true });
+const bot = new TelegramBot(botToken, { polling: true, request: { agent: agnet } });
+
 
 bot.onText(/\/start/, actions.startActions(bot));
 bot.on("callback_query", actions.callbackQueryActions(bot));
+bot.on('polling_error', function(err) {
+  throw err.message
+})
